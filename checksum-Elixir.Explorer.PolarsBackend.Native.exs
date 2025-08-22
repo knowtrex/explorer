d1d440e46b558a4fd5f9793331894e00344d4f05
Michał Szajbe <michal.szajbe@gmail.com>
2025-08-22 17:16:14 +0200

    Add v0.11.1-knowtrex.1 checksums

---
+++ checksum-Elixir.Explorer.PolarsBackend.Native.exs
@@ -0,0 +1,9 @@
+%{
+  "libexplorer-v0.11.1-knowtrex.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:92264edffe87cfa70a62c7add2b1bca658f7acc3233cd00a097feb72476ba5b5",
+  "libexplorer-v0.11.1-knowtrex.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:268b96710e22750fc1a1d161352e7c8894f401f76491b6fcda57a1f5973c6759",
+  "libexplorer-v0.11.1-knowtrex.1-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:a4754672291c7ee68744c0d7b9a0819f16f2b5147fd8cae176b19fbcd976e5aa",
+  "libexplorer-v0.11.1-knowtrex.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:d292c8c96b3f0379d2fe8ed6f9debddead9763c6a593f6870052ce4efe7ca29f",
+  "libexplorer-v0.11.1-knowtrex.1-nif-2.15-x86_64-unknown-linux-gnu--legacy_cpu.so.tar.gz" => "sha256:84e053fa162880aab34a8780f134e4e9a2f1a94268daeb2f37f867fa6a288650",
+  "libexplorer-v0.11.1-knowtrex.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:be911b5faa4e1a88ef0ee46d0df3ba68457a9991200496291f4a862d84f7b178",
+  "libexplorer-v0.11.1-knowtrex.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:9eae94ff0a4449e53a39193602ddfadaaca1449d1efcdfe5169745b7bc5ed7b1",
+}
